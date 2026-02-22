f6b50d3ae71b766fcfa1b471c6c1e48172473971
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-02-22 21:04:49 +0530

    .

---
+++ prime.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==2){
+            System.out.println("n is a prime number");
+        }
+        else{
+            for(int i=2;i<=n-1;i++){
+            if(i%n==0){
+                System.out.println("n is a prime number");
+                break;
+            }
+            else{
+                System.out.println("n is not prime number");
+                break;
+            }
+        }
+        }
+        
+    }
+}
