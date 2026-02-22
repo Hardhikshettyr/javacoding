eec4bc89dd8d2f7aaa161906483a5638ff5667df
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-02-22 21:10:03 +0530

    .

--- prime.java
+++ prime.java
@@ -9,7 +9,17 @@ public class prime {
             System.out.println("n is a prime number");
         }
         else{
-            for(int i=2;i<=n-1;i++){
+        //     for(int i=2;i<=n-1;i++){
+        //     if(i%n==0){
+        //         System.out.println("n is a prime number");
+        //         break;
+        //     }
+        //     else{
+        //         System.out.println("n is not prime number");
+        //         break;
+        //     }
+        //    }
+            for(int i=2;i<=Math.sqrt(n);i++){
             if(i%n==0){
                 System.out.println("n is a prime number");
                 break;
@@ -18,8 +28,7 @@ public class prime {
                 System.out.println("n is not prime number");
                 break;
             }
-        }
-        }
-        
+           }
+        }   
     }
 }
