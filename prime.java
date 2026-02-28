5d02d837d995297d59fb217586d755873c8c6e68
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-02-28 19:19:57 +0530

    .

--- prime.java
+++ prime.java
@@ -5,30 +5,18 @@ public class prime {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        if(n==2){
-            System.out.println("n is a prime number");
+        if(n<=1){
+            System.out.println("n is a not prime number");
         }
         else{
-        //     for(int i=2;i<=n-1;i++){
-        //     if(i%n==0){
-        //         System.out.println("n is a prime number");
-        //         break;
-        //     }
-        //     else{
-        //         System.out.println("n is not prime number");
-        //         break;
-        //     }
-        //    }
-            for(int i=2;i<=Math.sqrt(n);i++){
-            if(i%n==0){
-                System.out.println("n is a prime number");
-                break;
+            for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                System.out.println("n is a not prime number");
+                return;
             }
-            else{
-                System.out.println("n is not prime number");
-                break;
-            }
-           }
+            
         }   
     }
+    System.out.println("Prime number");
+}
 }
