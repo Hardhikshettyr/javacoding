3a1cf6fda61119ca56a193dee7671217db40442d
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-03-10 22:26:00 +0530

    .

---
+++ reversearray.java
@@ -0,0 +1,19 @@
+public class reversearray{
+    public static void reverse(int num[]){
+        int start=0,end=num.length-1;
+        while(start<end){
+            int temp=num[start];
+            num[start]=num[end];
+            num[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[]args){
+        int num[]={2,4,6,8,10};
+        reverse(num);
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i]+" ");
+        }
+    }
+}
