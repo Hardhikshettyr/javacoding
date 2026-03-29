160353bd8d3194875db5b3fff364c67de5623780
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-03-29 12:03:23 +0530

    .

---
+++ dsa/subarray.java
@@ -0,0 +1,17 @@
+public class subarray {
+    public static void sa(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr[k] + " ");
+                }
+                System.err.println("");
+            }
+            System.err.println("");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        sa(arr);
+    }
+}
