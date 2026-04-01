c1dd0f914293a48914988119733b74bae7b8bd12
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-04-01 05:39:08 +0530

    .

--- dsa/sort.java
+++ dsa/sort.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class sort{
     public static void bubblesort(int arr[]){
         for(int i=0;i<arr.length-1;i++){
@@ -40,6 +43,27 @@ public class sort{
             arr[prev+1]=curr;
         }
     }
+    
+    public static void countingsort(int arr[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest, arr[i]);
+        }
+
+        int count[]=new int [largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
     public static void dispaly(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
@@ -48,7 +72,9 @@ public class sort{
     }
     public static void main(String[]args){
         int arr[]={5,4,1,3,2};
-        insertionsort(arr);       
+        countingsort(arr);  
+        // Arrays.sort(arr);     
+        // Arrays.sort(arr,0,3);
         dispaly(arr);
     }
 }
