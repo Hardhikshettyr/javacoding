a3ee8c2a187b84cddb0964079704d0a83a84b46f
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-04-12 17:56:01 +0530

    .

--- 2darrays/search.java
+++ 2darrays/search.java
@@ -54,9 +54,26 @@ public class search {
         }
         return false;
     }
+
+    public static boolean staircase1(int matrix[][], int key){
+        int row=matrix.length-1;
+        int col=0;
+
+        while(row>=0 && col<matrix[0].length){
+            if(matrix[row][col]==key){
+                return true;
+            }else if(key<matrix[row][col]){
+                row--;
+            }else{
+                col++;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        boolean index=staircase(matrix,17);
+        boolean index=staircase(matrix,10);
         System.out.println(index);
     }
 }
