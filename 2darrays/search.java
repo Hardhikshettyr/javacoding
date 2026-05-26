aa91ebfa8fec8da35da761bfee5abbada1ec98d7
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-05-26 19:58:23 +0530

    .

--- 2darrays/search.java
+++ 2darrays/search.java
@@ -76,5 +76,6 @@ public class search {
         int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         boolean index=staircase1(matrix,10);
         System.out.println(index);
+        System.out.println(index);
     }
 }
