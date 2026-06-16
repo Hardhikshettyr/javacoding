dc3c40ac5e505034583fb29b5e8873c7fc7dae34
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-06-16 11:00:37 +0530

    .

--- java/one.java
+++
@@ -1,9 +0,0 @@
-public class one{
-    public static void main(String[] args) {
-        int count=1;
-        while(count<=10){
-            System.out.println(count);
-            count++;
-        }
-    }
-}
