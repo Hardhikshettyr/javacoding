69d1b21785e89f9d021f74717b5f58ccbd932bb4
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-06-16 13:21:45 +0530

    .

---
+++ Patterns/rectangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class rectangle {
+    public static void solution(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[]args){
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=Sc.nextInt();
+        solution(n);
+    }
+}
