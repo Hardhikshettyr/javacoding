63d5e76a6f0551f5be84282447fbf296e5cd3196
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-07-01 20:26:03 +0530

    solving sortin problems

---
+++ sorting/selectionsort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class selectionsort {
+    public static void sort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+
+                
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+            
+
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    public static void main(String[]args){
+        int arr[]={5,4,2,1,3};
+        sort(arr);
+    }
+}
