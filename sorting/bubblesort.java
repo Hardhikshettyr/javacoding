2e461a33e1af1dc949bdf2dcb98c7ebdcb3ef802
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-07-11 12:13:34 +0530

    problems solved on sorting

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -1,8 +1,8 @@
 public class bubblesort{
     public static void sort(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length-1;i++){ //for(int i=arr.length;i>0;i--)
             int swap=0;
-            for(int j=0;j<arr.length-1-i;j++){
+            for(int j=0;j<arr.length-1-i;j++){//for(int j=0;j<i;j++)
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
--- sorting/countingsort.java
+++ sorting/countingsort.java
@@ -4,9 +4,9 @@ public class countingsort {
     public static void sort(int arr[]){
         int largest=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-            largest=Math.max(largest,arr[i]);
+            largest=Math.max(arr[i],largest);
         }
-        int count[]=new int[largest+1];
+        int count[]=new int [largest+1];
         for(int i=0;i<arr.length;i++){
             count[arr[i]]++;
         }
@@ -19,7 +19,7 @@ public class countingsort {
             }
         }
         for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+            System.out.println(arr[i]);
         }
 
 
