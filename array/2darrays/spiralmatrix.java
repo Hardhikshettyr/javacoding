02224373ab07761b9c20dacc040bd77dae85a454
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-09-02 19:09:21 +0530

    Solving questions on arrays

--- array/2darrays/spiralmatrix.java
+++ array/2darrays/spiralmatrix.java
@@ -1,26 +1,28 @@
 public class spiralmatrix{
 
     public static void spiral(int matrix[][]){
-        int sr=0,sc=0;
-        int er=matrix.length-1, ec=matrix[0].length-1;
-        while(sr<=er && sc<=ec){
-            for(int i=sc;i<=ec;i++){
-                System.out.print(matrix[sr][i]+" ");
+        int startrow=0,startcol=0;
+        int endrow=matrix.length-1, endcol=matrix[0].length-1;
+
+        while(startrow<=endrow && startcol<=endcol){
+            for(int i=startcol;i<=endcol;i++){
+                System.out.println(matrix[startrow][i]+" ");
             }
-            for(int i=sr+1;i<=er;i++){
-                System.out.print(matrix[i][ec]+" ");
+            for(int i=startrow+1;i<=endrow;i++){
+                System.out.println(matrix[i][endcol]+" ");
             }
-            for(int i=ec-1;i>=sc;i--){
-                System.out.print(matrix[er][i]+" ");
+            for(int i=endcol-1;i>=startcol;i--){
+                System.out.println(matrix[endrow][i]+" ");
             }
-            for(int i=er-1;i>=sc+1;i--){
-                System.out.print(matrix[i][sc]+" ");
+            for(int i=endrow-1;i>=startrow+1;i--){
+                System.out.println(matrix[i][startcol]+" ");
             }
-            sr++;
-            sc++;
-            er--;
-            ec--;
+            startrow++;
+            endrow--;
+            startcol++;
+            endcol--;
         }
+        
     }
     public static void main(String[]args){
         int arr[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
