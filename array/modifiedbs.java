f6e14e403904577a24415e4b52eeefce8f7dcacd
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-09-08 09:22:50 +0530

    solving problems on array

--- array/modifiedbs.java
+++ array/modifiedbs.java
@@ -8,7 +8,7 @@ public class modifiedbs {
         if(num[mid]==target){
             return mid;
         }
-        if(num[start]>=num[mid]){
+        if(num[start]<=num[mid]){
             if(target>=num[start] && target<num[mid]){
                 end=mid-1;
             }else{
@@ -19,15 +19,15 @@ public class modifiedbs {
             if(target>num[mid] && target<=num[end]){
                 start=mid+1;
             }else{
-                end=mid-1;;
+                end=mid-1;
             }
         }
        }
        return -1;
     }
     public static void main(String[] args){
-        int a[]={4,5,6,0,1,2,3};
-        int target=1;
+        int a[]={4,5,6,7,0,1,2};
+        int target=0;
         int result=search(a,target);
         System.out.println(result);
     }
